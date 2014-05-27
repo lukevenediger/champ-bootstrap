12d231562b546bbc809e5e5d084cbf25ccd6c1fc
Richard Niemand <niemand.richard@gmail.com>
2014-05-27 21:02:54 +0200

    Updated all libs and template
    Added in utility JavaScript to add an active class to the current page

--- pages/blog/index.md
+++ pages/blog/index.md
@@ -5,8 +5,8 @@ title = My Blog
 
 Welcome to my blog!
 
-# [Article 2 - 2013-08-18](2013-08-18.html)
+# [Article 2 - 2014-05-27](2014-05-27.html)
 Turducken beef strip steak rump swine short loin jerky tri-tip bacon meatloaf shank filet mignon flank shoulder. Ham pork belly shankle, venison short loin biltong cow tail prosciutto jowl spare ribs sirloin pastrami swine. Short loin tenderloin filet mignon ham, flank shank strip steak. Strip steak pork drumstick turducken andouille pork chop shank shoulder flank....
 
-# [Article 1 - 2013-08-17](2013-08-17.html)
+# [Article 1 - 2014-05-26](2014-05-26.html)
 Spare ribs dolore t-bone pancetta ut. Sunt mollit pork est, biltong fatback nisi chicken pancetta. Fugiat sirloin venison officia dolore jowl aliqua filet mignon excepteur pork. Aliquip proident chuck, in doner eu flank laboris sed ham enim tail shank. Ut ad veniam shank ham....
---
+++ static/scripts/champ-bootstrap.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    // Simple shim to mark the current page as active
+    (function(){
+        var pageUrl = location.href, linkFound = false;
+        // Attempt 1 - Check the top level page URLS
+        $('ul.nav > li').each(function(idx, li){
+            if(linkFound) return;
+            var linkHref = $(li).find('a').attr('href').replace('../', '');
+            if(pageUrl.indexOf(linkHref) >= 0) {
+                $(li).addClass('active');
+                linkFound = true;
+            }
+        });
+        
+        // Attempt 2 - Check child pages
+        if(!linkFound) {
+            pageUrl = location.pathname;
+            $('ul.nav > li').each(function(idx, li){
+                if(linkFound) return;
+                var linkHref = $(li).find('a').attr('href').replace('../', '');
+                var baseUrl = linkHref.substring(0, linkHref.lastIndexOf('/') + 1);
+                if(pageUrl.indexOf(baseUrl) >= 0) {
+                    $(li).addClass('active');
+                    linkFound = true;
+                }
+            }); 
+        }
+    }());
+});
