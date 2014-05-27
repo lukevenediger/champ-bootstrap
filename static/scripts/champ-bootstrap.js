bc654756c9c4347cf807341b7b79e9f1afd6740a
Richard Niemand <niemand.richard@gmail.com>
2014-05-27 22:03:14 +0200

    Added highlighting support for drop downs

--- static/scripts/champ-bootstrap.js
+++ static/scripts/champ-bootstrap.js
@@ -6,10 +6,7 @@ $(document).ready(function(){
         $('ul.nav > li').each(function(idx, li){
             if(linkFound) return;
             var linkHref = $(li).find('a').attr('href').replace('../', '');
-            if(pageUrl.indexOf(linkHref) >= 0) {
-                $(li).addClass('active');
-                linkFound = true;
-            }
+            if(pageUrl.indexOf(linkHref) >= 0) { $(li).addClass('active'); linkFound = true; }
         });
         
         // Attempt 2 - Check child pages
@@ -19,10 +16,18 @@ $(document).ready(function(){
                 if(linkFound) return;
                 var linkHref = $(li).find('a').attr('href').replace('../', '');
                 var baseUrl = linkHref.substring(0, linkHref.lastIndexOf('/') + 1);
-                if(pageUrl.indexOf(baseUrl) >= 0) {
-                    $(li).addClass('active');
-                    linkFound = true;
-                }
+                if(baseUrl.length > 0 && pageUrl.indexOf(baseUrl) >= 0) { $(li).addClass('active'); linkFound = true; }
+            }); 
+        }
+        
+        // Attempt 3 - Check drop downs
+        if(!linkFound) {
+            $('ul.nav > li > ul > li').each(function(idx, li){
+                if(linkFound) return;
+                var linkHref = $(li).find('a').attr('href').replace('../', '');
+                var baseUrl = linkHref.substring(0, linkHref.lastIndexOf('/') + 1);
+                if(pageUrl.indexOf(baseUrl) >= 0) { $(li).parent().parent().addClass('active');}
+                if(pageUrl.indexOf(linkHref) >= 0){ $(li).addClass('active'); linkFound = true; }
             }); 
         }
     }());
